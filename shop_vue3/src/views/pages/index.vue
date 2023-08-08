213729eb0692550fb717f94308b1a641e5acc33a
blair-chuck <15776997314@163.com>
2023-08-08 15:24:17 +0800

    提交文件

---
+++ shop_vue3/src/views/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>欢迎来到用户管理系统</h1>
+    </div>
+</template>
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:"roles"
+}
+</script>
